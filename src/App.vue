86639d628c7a6f8aef95eeb0be4c6931c09ebd6e
aoshisen <aoshisen@fanxiapp.com>
2024-09-27 10:16:56 +0800

    :tada: first init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import TestComponent from './components/TestComponent.vue';
+import { computed } from 'vue';
+
+let X = 200;
+let rotationAngle = 45; // 新增：定义旋转角度
+
+// 计算调整后的 margin-left
+const adjustedMarginLeft = computed(() => {
+  // 将角度转换为弧度
+  const radians = (rotationAngle * Math.PI) / 180;
+  // 计算调整后的 margin-left
+  return X / Math.cos(radians);
+});
+</script>
+
+<template>
+  <div class="main">
+    <TestComponent :rotationAngle="rotationAngle" />
+    <TestComponent :marginLeft="adjustedMarginLeft" :rotationAngle="rotationAngle" />
+  </div>
+</template>
+
+<style scoped>
+.main {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  marginLeft?: number;
+  rotationAngle: number;
+}>();
+</script>
+
+<template>
+  <div class="test"
+    :style="{ marginLeft: marginLeft ? `${marginLeft}px` : undefined, transform: `rotate(${rotationAngle}deg)` }">
+    <div class="test-content"></div>
+  </div>
+</template>
+
+<style scoped>
+.test {
+  width: 100px;
+  border: 1px solid #000;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.test-content {
+  width: 50px;
+  height: 100%;
+  background-color: #3498db;
+}
+</style>
